eaead5fcd0b83c02309cae32833907d39f928d21
Josep <josep.arpi@gmail.com>
2017-09-24 12:24:38 +0200

    Feature Aggregation - GroupBy

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 function query() {
 
-    this.filter = function(o) { return o }
-    this.from = []
-	this.filterCallable = true
+    this.selectValue = function(o) { return o }
+    this.whereValue = function(o) { return o }
+    this.groupValue = function(o) { return o }
+    this.fromValue = []
 	this.fromCalls = 0
 	this.selectCalls = 0
+	this.groupCalls = 0
 
     this.select = function(f) {
-		this.filter = f || this.filter
+		this.selectValue = f || this.selectValue
 		this.selectCalls++
         return this
     }
 
     this.from = function(v) {
-        this.from = v
+        this.fromValue = v
 		this.fromCalls++
         return this
     }
 
+	this.where = function(v) {
+		this.whereValue = v
+		return this
+	}
+
+	this.groupBy = function(v) {
+		this.groupValue = v
+		this.groupCalls++
+		return this
+	}
+
     this.execute = function() {
 		if (this.selectCalls > 1) throw new Error('Duplicate SELECT')
 		if (this.fromCalls > 1) throw new Error('Duplicate FROM')
-		return this.from.map(this.filter)
+		return this.fromValue
+				// Where
+				.filter(this.whereValue)
+				// Select
+				.map(this.selectValue)
+				// GroupBy
+				.reduce((prev, o) => { if (prev.indexOf(this.groupValue(o)) === -1){ prev.push(this.groupValue(o));} return prev},[])
+.map((o) => {return [o, this.fromValue.filter((oo)=> {return this.groupValue(oo) === o})]})
     }
 
     return this
--- specs/index.js
+++ specs/index.js
@@ -37,9 +37,60 @@ describe('SQL - like parser', function() {
 			assert.throws(() => { query().select().select().from().execute() }, 'Duplicate SELECT')
 		})
 
+		it('Should throw exception if FROM called multiple times', function() {
+			assert.throws(() => { query().select().from([]).from([]).execute() }, 'Duplicate FROM')
+		})
+
+		it('Can omit FROM clause', function() {
+			assert.deepEqual(query().select().execute(), [])
+		})
+
+		it('Can omit SELECT clause', function() {
+			assert.deepEqual(query().from(nums).execute(), nums)
+		})
+
+		it('Should WHERE clause apply filter with SELECT by obj field', function() {
+			function isTeacher(o) { return o.profession === 'teacher' }
+			function profession(o) { return o.profession }
+			const expectedObjs = ['teacher', 'teacher', 'teacher']
+			assert.deepEqual(query().select(profession).from(objs).where(isTeacher).execute(), expectedObjs)
+		})
+
+		it('Should WHERE clause apply filter without SELECT', function() {
+			function isTeacher(o) { return o.profession === 'teacher' }
+			const expectedObjs = [{name: 'Peter', profession: 'teacher', age: 20, maritalStatus: 'married'},
+			  {name: 'Michael', profession: 'teacher', age: 50, maritalStatus: 'single'},
+			  {name: 'Peter', profession: 'teacher', age: 20, maritalStatus: 'married'}]
+			assert.deepEqual(query().select().from(objs).where(isTeacher).execute(), expectedObjs)
+		})
+
+		it('Should GROUPBY apply', function() {
+			function profession(o) { return o.profession }
+			const expectedObjs =
+      [
+          ['teacher',
+            [
+              {name: 'Peter', profession: 'teacher', age: 20, maritalStatus: 'married'},
+              {name: 'Michael', profession: 'teacher', age: 50, maritalStatus: 'single'},
+              {name: 'Peter', profession: 'teacher', age: 20, maritalStatus: 'married'}
+            ]
+          ],
+          ['scientific',
+            [
+              {name: 'Anna', profession: 'scientific', age: 20, maritalStatus: 'married'},
+              {name: 'Rose', profession: 'scientific', age: 50, maritalStatus: 'married'},
+              {name: 'Anna', profession: 'scientific', age: 20, maritalStatus: 'single'},
+            ]
+          ],
+          ['politician',
+            [
+              {name: 'Anna', profession: 'politician', age: 50, maritalStatus: 'married'}
+            ]
+          ]
+      ]
+			assert.deepEqual(query().select().from(objs).groupBy(profession).execute(), expectedObjs)
+		})
+
+
 	})
 })
-
-
-
-
